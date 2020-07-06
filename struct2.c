549482c5079a88a105590ce4fc106406475467a9
vishalgaurav28 <67232758+vishalgaurav28@users.noreply.github.com>
2020-07-06 14:01:05 -0700

    Create struct2.c

---
+++ struct2.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+struct books {
+    char title[50];
+    char author [50];
+    char subject[100];
+    int book id;
+};
+int main(){
+struct books book 1;
+struct books book2;
+strcpy(book1.title,"c programming");
+strcpy(book1.author,"vishal gaurav");
+strcpy(book1.subject,"c programming tutorial");
+book1.book id=9661432279;
+
+strcpy(book2.title,"EVS");
+strcpy(book2.author,"abhijeet");
+strcpy(book2.subject,"EVS tutorial");
+book2.book id=7033345171;
+
+printf("book 1 title:%s\n",book1.title);
+printf("book 1 author:%s\n",book1.author);
+printf("book 1 subject:%s\n",book1.subject);
+printf("book 1 book id:%d\n",book1.book id);
+
+printf("book 2 title:%s\n",book2.title);
+printf("book 2 author:%s\n",book2.author);
+printf("book 2 subject;%s\n",book2.subject);
+printf("book 2 book id:%d\n",book2.book id);
+return o;
+}
